79446b6ab036518873f0afb00a490c4e609bf697
Alejandro Magana <20.alejandro.magana@ksd.org>
2018-10-03 09:10:24 -0700

    adding enemy

---
+++ SpookyScaryHouse/Assets/Scripts/PlayerHP.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerHP : MonoBehaviour {
+    public int hp = 10;
+    public Text healthText;
+    public Slider healthbar;
+
+    void Start()
+    {
+        healthText.GetComponent<Text>().text = "Health:" + hp;
+        healthbar.GetComponent<Slider>().value = hp;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            hp--;
+            healthText.GetComponent<Text>().text = "Health:" + hp;
+            healthbar.GetComponent<Slider>().value = hp;
+            if (hp <= 0)
+            {
+                //Reload the level
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
---
+++ SpookyScaryHouse/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+    public float shootSpeed = 10;
+    public GameObject prefab;
+    float timer = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+		if(Input.GetButton("Fire1")&& timer > 0.2f)
+        {
+            timer = 0;
+            var mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            mousePosition.z = 0;
+            Vector3 shootDir = mousePosition - transform.position;
+            shootDir.Normalize();
+            Vector3 offset = shootDir;
+            shootDir = shootDir * shootSpeed;
+            //When calculating a Vector from a to b
+            //always do destination - start position
+            GameObject bullet = (GameObject)Instantiate(prefab, transform.position, Quaternion.identity);
+            bullet.GetComponent<Rigidbody2D>().velocity = shootDir;
+            Destroy(bullet, 0.5f);
+        }
+	}
+}
