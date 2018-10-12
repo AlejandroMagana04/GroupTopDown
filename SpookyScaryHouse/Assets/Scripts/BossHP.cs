c55ae22bc8c6e4f95f772223dd5ee2210351d8da
Kent Melville <19.kent.melville@ksd.org>
2018-10-12 10:20:56 -0700

    ZombieDuckBoss

---
+++ SpookyScaryHouse/Assets/Scripts/BossHP.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHP : MonoBehaviour
+{
+    public int hp = 10;
+    public GameObject door;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "bullet")
+        {
+            hp--;
+            if (hp <= 0)
+            {
+                Destroy(gameObject);
+                door.SetActive(false);
+            }
+        }
+    }
+
+}
