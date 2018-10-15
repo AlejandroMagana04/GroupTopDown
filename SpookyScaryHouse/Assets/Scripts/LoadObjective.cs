7e1d773906bd524d178a2d03307ac0e264cd83c7
Kent Melville <19.kent.melville@ksd.org>
2018-10-15 10:28:11 -0700

    updated game

---
+++ SpookyScaryHouse/Assets/Scripts/LoadObjective.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadObjective : MonoBehaviour {
+
+	public void Objective()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Objective");
+    }
+}
