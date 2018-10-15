d8f8c2342c60d646466c402ddfef899483331d08
Kent Melville <19.kent.melville@ksd.org>
2018-10-15 08:36:37 -0700

    updated game

--- SpookyScaryHouse/Assets/Scripts/DoorAppear.cs
+++ SpookyScaryHouse/Assets/Scripts/DoorAppear.cs
@@ -8,6 +8,7 @@ public class DoorAppear : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.gameObject.tag == "Player")
         door.SetActive(true);
     }
 }
